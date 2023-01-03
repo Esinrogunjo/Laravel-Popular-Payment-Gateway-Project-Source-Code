fe8e42d9bc003a4047f0263b5755d7460773d0d9
shakilahmed0369 <sakilhossain01969@gmail.com>
2023-01-03 18:27:10 +0600

    Integrate Stripe Gateway In Laravel

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\Gateways\PaypalController;
+use App\Http\Controllers\Gateways\StripeController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -24,3 +25,7 @@ Route::post('paypal/payment', [PaypalController::class, 'payment'])->name('paypa
 Route::get('paypal/success', [PaypalController::class, 'success'])->name('paypal.success');
 Route::get('paypal/cancel', [PaypalController::class, 'cancel'])->name('paypal.cancel');
 
+
+Route::post('stripe/payment', [StripeController::class, 'payment'])->name('stripe.payment');
+Route::get('stripe/success', [StripeController::class, 'success'])->name('stripe.success');
+Route::get('stripe/cancel', [StripeController::class, 'cancel'])->name('stripe.cancel');
